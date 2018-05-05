183594e00ff79a368c91e28c66bae6ec4d284c1f
Daniel <daniel2.meier@tu-dortmund.de>
2018-05-05 18:04:28 +0200

    commit

--- VolumeControl/AudioSessionManager.cpp
+++ VolumeControl/AudioSessionManager.cpp
@@ -2,11 +2,15 @@
 #include "AudioSessionManager.h"
 
 #include <iostream>
+#include <Psapi.h>
+#include <string>
+
+#define BUFFER_STRING_SIZE 100U
 
 using std::map;
 using std::shared_ptr;
 using std::vector;
-using std::string;
+using std::wstring;
 using std::make_shared;
 
 AudioSessionManager::AudioSessionManager(VolumeController& volController): 
@@ -33,12 +37,12 @@ shared_ptr<vector<UINT>> AudioSessionManager::getSessionIDs() const {
 	return keySet;
 }
 
-shared_ptr<string> AudioSessionManager::getSessionNameByID(UINT id) const {
+shared_ptr<wstring> AudioSessionManager::getSessionNameByID(const UINT id) const {
 	const auto find = idMap->find(id);
 	
 	if(find != idMap->end()) {
 		
-		IAudioSessionControl2* audioSessionControl = find->second;
+		const auto audioSessionControl = find->second;
 
 		ASSERT(audioSessionControl);
 
@@ -47,9 +51,7 @@ shared_ptr<string> AudioSessionManager::getSessionNameByID(UINT id) const {
 		const auto hr = audioSessionControl->GetProcessId(&pid);
 
 		if(SUCCEEDED(hr)) {
-			return make_shared<string>("");
-
-
+			return getProcessNameByPID(pid);
 		}
 
 		DEBUG_ERR_PRINT("Failed to get PID from audio session! Err code:" << hr);
@@ -58,3 +60,89 @@ shared_ptr<string> AudioSessionManager::getSessionNameByID(UINT id) const {
 	return nullptr;
 }
 
+IAudioSessionControl2* AudioSessionManager::getSessionByID(UINT id) const {
+
+}
+
+void AudioSessionManager::removeSessionByID(const UINT id) const {
+	const auto searchIdMap = idMap->find(id);
+	const auto searchHandlerMap = eventHandlerMap->find(id);
+
+	/// found in map 1 gdw. found in map 2
+	ASSERT((searchIdMap == idMap->end()) == (searchHandlerMap == eventHandlerMap->end()));
+
+	if(searchIdMap == idMap->end() && searchHandlerMap == eventHandlerMap->end()) {
+		return;
+	}
+
+	/// remove from map
+	idMap->erase(searchIdMap);
+
+	/// release event handler and session
+	const auto sessionEventHandler = searchHandlerMap->second;
+	const auto session = searchIdMap->second;
+
+	ASSERT(session && sessionEventHandler);
+
+	if(session && sessionEventHandler) {
+		session->UnregisterAudioSessionNotification(sessionEventHandler);
+
+		SAFERELEASE(sessionEventHandler);
+		SAFERELEASE(session);
+	}
+}
+
+shared_ptr<wstring> AudioSessionManager::getProcessNameByPID(DWORD pid) const {
+	auto returnStr = make_shared<wstring>();
+	
+	const auto processName = static_cast<LPTSTR>(malloc(BUFFER_STRING_SIZE));
+
+	const auto handle = OpenProcess(
+		PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+		FALSE,
+		pid
+	);
+
+	if (handle) {
+
+		/// try to get the process name
+		const auto copiedChars = GetModuleBaseName(
+			handle,
+			nullptr,
+			processName,
+			BUFFER_STRING_SIZE
+		);
+
+		/// if GetModuleBaseName returns 0 a error occurred
+		/// print it
+		if(!copiedChars) {
+			const auto errorCode = GetLastError();
+			DEBUG_PRINT("Error retrieving the Process name of process with PID " << pid << ". Error Code: " << std::hex << HRESULT_FROM_WIN32(errorCode));
+
+			returnStr->append(L"System");
+		} else {
+			returnStr->append(processName, copiedChars);
+		}		
+
+	} else {
+		DEBUG_ERR_PRINT("Error getting ProcessHandle");
+
+		returnStr->append(L"System");
+	}
+
+	CloseHandle(handle);
+
+	free(processName);
+
+	/// cut the .exe extension
+	const auto fileExtensionIndex = returnStr->find_last_of('.');
+
+	if (fileExtensionIndex < wstring::npos) {
+		const auto str = returnStr->substr(0, fileExtensionIndex);
+
+		returnStr->clear();
+		returnStr->append(str);
+	}
+
+	return returnStr;
+}
--- VolumeControl/AudioSessionManager.h
+++ VolumeControl/AudioSessionManager.h
@@ -15,11 +15,19 @@ private:
 	const VolumeController& volController;
 
 	const std::shared_ptr<std::map<UINT, IAudioSessionControl2*>> idMap;
-	const std::shared_ptr<std::map<UINT, AudioSessionEventHandler&>> eventHandlerMap;
+	const std::shared_ptr<std::map<UINT, AudioSessionEventHandler*>> eventHandlerMap;
+
 	
+
 	UINT currentID;
 
 	UINT getNextID();
+
+	/// --- Private Functions ---
+
+	// returns a smart pointer to a string containing the process name
+	// throws a exception when it's not able to retrieve the name
+	std::shared_ptr<std::wstring> getProcessNameByPID(DWORD pid) const;
 
 public:
 	AudioSessionManager(VolumeController& volController);
@@ -29,13 +37,14 @@ public:
 	std::shared_ptr<std::vector<UINT>> getSessionIDs() const;
 
 	// returns the name of the audio session
-	std::shared_ptr<std::string> getSessionNameByID(UINT id) const;
+	// throws a exception when it's not able to retrieve the name
+	std::shared_ptr<std::wstring> getSessionNameByID(UINT id) const;
 
 	// returns the object to control the volume
 	IAudioSessionControl2* getSessionByID(UINT id) const;
 
 	// remove a audio session when it's no longer active
-	void removeSessionByID(UINT id);
+	void removeSessionByID(UINT id) const;
 
 	// release all references
 	void shutdown(void);
